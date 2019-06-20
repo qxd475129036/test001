ca65d9551a480b08d465263e0e7b921e01c11fef
qxd <475129036@qq.com>
2019-06-20 22:07:05 +0800

    spring boot about web technology

---
+++ qxd-spring-boot-web/src/main/java/com/spring/boot/web/qxdspringbootweb/QxdSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.web.qxdspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QxdSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QxdSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ qxd-spring-boot-web/src/test/java/com/spring/boot/web/qxdspringbootweb/QxdSpringBootWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.boot.web.qxdspringbootweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QxdSpringBootWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
